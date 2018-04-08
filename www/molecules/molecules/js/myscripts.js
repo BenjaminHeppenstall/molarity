4b66196c38fd7b9c7aeb76b010611712c114fb45
Benjamin Heppenstall <37471480+BenjaminHeppenstall@users.noreply.github.com>
2018-04-08 23:00:27 +0200

    update

--- www/molecules/molecules/js/myscripts.js
+++ www/molecules/molecules/js/myscripts.js
@@ -3,6 +3,14 @@ var input1 = document.getElementById("mol");
 input1.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
+        document.getElementById("notation").focus();
+    }
+});
+
+var input = document.getElementById("notation");
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
         document.getElementById("calcBtn").click();
     }
 });
@@ -12,6 +20,7 @@ function calculateMolecules() {
 	var power = Math.pow(10, 23);
 
 	var amount = parseFloat(document.getElementById("mol").value, 10);
+	var size = parseFloat(document.getElementById("notation").value, 10);
 
 	if (isNaN(amount) || amount <= 0) { 
 
@@ -24,8 +33,20 @@ function calculateMolecules() {
 
 	    var answer = power * answer1;
 
-		document.getElementById("answer").innerHTML = answer + " molecules";
+	    if (isNaN(size)) { 
+
+	    	document.getElementById("answer").innerHTML = answer + " molecules";
+
+	    } else if (size == 0) {
+
+	    	document.getElementById("answer").innerHTML = "0 molecules";
+
+	    } else {
+
+		document.getElementById("answer").innerHTML = answer * Math.pow(10, size) + " molecules";
 		//document.getElementById("answer").innerHTML = "works";
+
+		}
 		
 	}
 
